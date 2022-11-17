2a3485499c65a6127053980eeb5598e582950457
celeste-42bit <studi.jasha.gruener@gmail.com>
2022-11-17 18:00:34 +0100

    modified:   Blatt 4/Blatt4.rkt

--- Blatt 4/Blatt4.rkt
+++ Blatt 4/Blatt4.rkt
@@ -12,11 +12,13 @@
     (date-mon natural)
     (date-yea natural))
 
-(define)
+(define first_date (make-calendar-date 12 11 2022))
+(define second_date (make-calendar-date 06 03 1921))
+(define illegal_date (make-calendar-date 69 666 420))
 
 (check-within (date-day calendar-date) 1 30)
 (check-within (date-mon calendar-date) 1 12)
-(check-within (date-yea calendar-date) 0 2022)
+(check-within (date-yea calendar-date) 0 2023)
 
 (define calendar-date-ok?
     (lambda (date)
