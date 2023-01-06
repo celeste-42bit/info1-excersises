d124a4c4594d1b2e00899683a66162c70f512733
celeste-42bit <studi.jasha.gruener@gmail.com>
2023-01-06 17:30:32 +0100

    Started assignment 9

---
+++ Blatt 9/Blatt9_1.rkt
@@ -0,0 +1,17 @@
+;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
+;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
+#reader(lib "vanilla-reader.rkt" "deinprogramm" "sdp")((modname Blatt9_1) (read-case-sensitive #f) (teachpacks ((lib "image.rkt" "teachpack" "deinprogramm" "sdp") (lib "universe.rkt" "teachpack" "deinprogramm" "sdp"))) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ((lib "image.rkt" "teachpack" "deinprogramm" "sdp") (lib "universe.rkt" "teachpack" "deinprogramm" "sdp")))))
+; Assignment 1
+
+; Input & Tests
+(: walls (list-of natural))
+(define walls (list 1 8 6 2 5 4 8 3 7))
+
+#| (check-expect (max-volume walls) 49) |#
+
+; a) Function max-volume
+(: max-volume ((list-of natural) -> natural))
+(define max-volume
+  (lambda (walls)
+    (cons
+    ...))
